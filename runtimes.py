597f25c8b6b27b4d5b630f7e01616f64cc733d09
npcrites <nicholascrites22@gmail.com>
2023-03-03 15:05:29 -0800

    pushing initial table

--- runtimes.py
+++ runtimes.py
@@ -20,6 +20,8 @@ if __name__ == '__main__':
     parser.add_argument('--input', choices=['sorted', 'random'], default='random')
     args = parser.parse_args()
 
+    print(f'|                |       timsort       |   merge_sorted  |  quick_sorted    |')
+    print(f'|----------------|---------------------|-----------------|------------------|')
     # perform the runtime tests
     for x in range(0, args.max_x+1):
 
@@ -55,7 +57,8 @@ if __name__ == '__main__':
         # You will have to look up how to do this formatting.
         # In order to get a proper markdown table,
         # you will have to also print a header line somewhere else.
-        print(f'len(xs)=2**{x} runtimes={runtimes}')
+        print(f"| n**{x:0.2e}    |    {runtimes['timsort']:0.2e}         |    {runtimes['merge_sorted']:0.2e}     |   {runtimes['quick_sorted']:0.2e}       |")
+
 
         # HINT:
         # use f-strings and a print statement that looks something like
